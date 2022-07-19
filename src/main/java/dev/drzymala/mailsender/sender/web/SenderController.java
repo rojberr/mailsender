0215948b03499701bff57f475c377562b30a1445
Rojberr <rojberr@outlook.com>
2022-07-19 18:46:42 +0200

    build(heroku): Add system.properties with jvm version for Heroku deploy

--- src/main/java/dev/drzymala/mailsender/sender/web/SenderController.java
+++ src/main/java/dev/drzymala/mailsender/sender/web/SenderController.java
@@ -4,7 +4,6 @@ import dev.drzymala.mailsender.sender.application.port.SenderUseCase;
 import dev.drzymala.mailsender.sender.domain.MyUser;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
